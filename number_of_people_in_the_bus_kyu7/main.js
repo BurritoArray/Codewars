9980b75646cfcd136a8999dece96172af9b96d3d
Brian <bshaw111786@gmail.com>
2023-02-28 13:38:36 -0600

    modified challenge. previous attempt did not work for all test cases. nested loop wasn't necessary

--- number_of_people_in_the_bus_kyu7/main.js
+++ number_of_people_in_the_bus_kyu7/main.js
@@ -13,20 +13,13 @@
 function number (busStops) {
     let numOnBus = 0
     for (let i = 0; i < busStops.length; i++) {
-        for (let j = 0; j < busStops[i].length; j++) {
-                if (busStops[i][j] === busStops[i][0]) {
-                    numOnBus += busStops[i][j]
-                } else {
-                    numOnBus -= busStops[i][j]
-                }
-        }
+        numOnBus += busStops[i][0]
+        numOnBus -= busStops[i][1]
     } return numOnBus
 }
 
-
-
-
+console.log(number([[28,0],[9,6],[37,14],[74,35],[92,49],[70,70],[62,112],[73,76],[45,45],[83,64],[46,84],[50,48],[76,42],[12,82],[57,20],[58,34],[62,5]]))
 console.log(number([[10,0],[3,5],[5,8]]))
-console.log(number([[3,0],[9,1],[4,10],[12,2],[6,1],[7,10]]),17);
-console.log(number([[3,0],[9,1],[4,8],[12,2],[6,1],[7,8]]),21);
-console.log(number([[0,0]]),0);
+console.log(number([[3,0],[9,1],[4,10],[12,2],[6,1],[7,10]]))
+console.log(number([[3,0],[9,1],[4,8],[12,2],[6,1],[7,8]]))
+console.log(number([[0,0]]))
