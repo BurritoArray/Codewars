6e719af8c771f13dbcb2c8f7ba6a80488e00e41c
Brian <bshaw111786@gmail.com>
2023-02-28 13:39:59 -0600

    modified challenge. previous attempt did not work for all test cases. nested loop wasn't necessary

--- number_of_people_in_the_bus_kyu7/main.js
+++ number_of_people_in_the_bus_kyu7/main.js
@@ -23,3 +23,4 @@ console.log(number([[10,0],[3,5],[5,8]]))
 console.log(number([[3,0],[9,1],[4,10],[12,2],[6,1],[7,10]]))
 console.log(number([[3,0],[9,1],[4,8],[12,2],[6,1],[7,8]]))
 console.log(number([[0,0]]))
+//update
