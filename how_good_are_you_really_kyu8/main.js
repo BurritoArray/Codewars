f6a302994a570d8b2d0770e9fe16b96cdaad5019
Brian <bshaw111786@gmail.com>
2023-03-23 21:12:31 -0500

    Completed codewar. Added #1 solution. Linked js with html to load into console.

--- how_good_are_you_really_kyu8/main.js
+++ how_good_are_you_really_kyu8/main.js
@@ -9,10 +9,25 @@
 // Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
 
 function betterThanAverage(classPoints, yourPoints) {
-  
+  const sumClassPoints = classPoints.reduce((a, c) => a + c, 0);
+  const avgScores = sumClassPoints / classPoints.length
+  console.log(avgScores, yourPoints)
+  if (avgScores > yourPoints) {
+    return false
+  } else {
+    return true
+  }
 }
-
+//Plenty of room for improvement here.
 
 console.log(betterThanAverage([2, 3], 5)/*, true*/);
 console.log(betterThanAverage([100, 40, 34, 57, 29, 72, 57, 88], 75)/*, true*/);
 console.log(betterThanAverage([12, 23, 34, 45, 56, 67, 78, 89, 90], 9)/*, false*/);
+
+
+
+// #1 solution
+// function betterThanAverage(classPoints, yourPoints) {
+//     return yourPoints > classPoints.reduce((a, b) => a + b, 0) / classPoints.length; 
+//   }
+  
