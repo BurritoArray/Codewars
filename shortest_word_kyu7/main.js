35d13b744f7cd52badce0f07a16467423b08068e
Brian <bshaw111786@gmail.com>
2023-03-29 00:43:35 -0500

    Completed daily challenge.

--- shortest_word_kyu7/main.js
+++ shortest_word_kyu7/main.js
@@ -3,13 +3,38 @@
 // String will never be empty and you do not need to account for different data types.
 
 function findShort(s){
-    
+    let shortWord = 0;
+    let rando = s.split(' ').forEach(element => {
+        
+        if (shortWord === 0) {
+            shortWord = element.length
+            
+        } else if (shortWord > element.length) {
+            shortWord = element.length
+            
+            
+        }
+    });
+    return shortWord
 }
 
 
 console.log(findShort("bitcoin take over the world maybe who knows perhaps"))
 // , 3);
 console.log(findShort("turns out random test cases are easier than writing out basic ones"))
-// , 3); 
+// // , 3); 
 console.log(findShort("Let's travel abroad shall we"))
-// , 2);
+// // , 2);
+
+
+//Top answers on Codewars
+
+// function findShort(s){
+//     return Math.min.apply(null, s.split(' ').map(w => w.length));
+//   }
+
+
+
+// function findShort(s){
+//     return Math.min(...s.split(" ").map (s => s.length));
+// }
